7bf29101b756ba0d6bdec5f9c7a1c986f344915c
tim.hennessey <Tim.Hennessey@deutschinc.com>
2020-09-09 09:38:53 -0700

    Initial Commit

---
+++ app/300x250_1/scripts/Animation.js
@@ -0,0 +1,47 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var tl = new TimelineMax();
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+		tl.from(hero, .5, {x:'-=300', ease: Sine.easeOut})
+		.from(overlay1, .5, {autoAlpha:0}, "-=.15")
+		.from(overlay2, .5, {autoAlpha:0}, "-=.5")
+		.from(overlay3, .5, {autoAlpha:0})
+		
+		.from(sticker, .25, {autoAlpha:0, rotation:-18, scale:1.5})
+		.from(overlay4, .5, {autoAlpha:0});
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
